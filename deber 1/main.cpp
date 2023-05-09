2a2d2542ad7e6057893e21a3e526eaf1655d18f2
Jaime Reyes <jaime.jr1972@hotmail.com>
2023-05-09 15:30:36 -0500

    main

---
+++ deber 1/main.cpp
@@ -0,0 +1,11 @@
+#include"fraccion.cpp"
+#include<iostream>
+#include<stlib.h>
+using namespace std;
+int main(){
+    cout <<"Escriba el numerador numero 1"<<;
+    cin << numerador1<<;
+    cout <<"Escriba el numerador 2";
+    cin << numerador<<;
+
+}

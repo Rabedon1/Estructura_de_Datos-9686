d48f8a8d853d4ef58f57239b2ba0b8f7bdd282da
Raúl Andrés Silva Velásquez <99281741+RansilvaV29@users.noreply.github.com>
2023-06-06 16:47:10 -0500

    Update Aplicativo.cpp

--- Parcial 1/Deber 5 Listas Simples/Aplicativo.cpp
+++ Parcial 1/Deber 5 Listas Simples/Aplicativo.cpp
@@ -3,8 +3,7 @@
  * Nombres: Roberto Bedon, Jaime Reyes, Raul Silva.
  * Fecha de creacion: 29/05/23 20:32
  * Fecha de modificacion: 06/06/23 13:30
- * Enunciado: Realice operaciones con sobrecargas.
- * Tema: operadores sobrecargados
+ * Tema: Listas Simples 
  *******************************************************************************/ 
 
 #include <iostream>
